6baea1750bfcf0b9312bdcf1be8f91e7bb6ce07c
Ethan-Vanderheijden <ethanvanderheijden@gmail.com>
2024-05-30 16:14:21 -0700

    Patch aiosqlite to run threads as daemonic.

--- partners_list/models/base_db.py
+++ partners_list/models/base_db.py
@@ -1,3 +1,24 @@
+from aiosqlite.core import Connection
+from queue import SimpleQueue
+
+
+# By default, aiosqlite spawns threads as non-daemonic which keeps the program
+# alive even after the main thread has finished.
+# Ideally, you should manually close the aiosqlite connection, but letting the
+# connection expire by killing the program should be safe.
+# relevant: https://github.com/omnilib/aiosqlite/issues/299
+def _patched_init(self, connector, iter_chunk_size):
+    super(Connection, self).__init__(daemon=True)
+    self._running = True
+    self._connection = None
+    self._connector = connector
+    self._tx = SimpleQueue()
+    self._iter_chunk_size = iter_chunk_size
+
+
+Connection.__init__ = _patched_init
+
+
 import aiosqlite
 from contextlib import asynccontextmanager
 
